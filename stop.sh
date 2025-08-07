fb17d5995e9e73374aa4659d481b2bc0dd7ec485
AlterStrong <kucing.lucuuuu12@gmail.com>
2025-08-07 20:57:50 +0700

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,4 +1,3 @@
 #!/data/data/com.termux/files/usr/bin/bash
-# Hentikan semua proses roblox_monitor.sh
-
-pkill -f roblox_monitor.sh && echo "Script dihentikan." || echo "Script tidak ditemukan."
+pkill -f roblox_monitor.sh
+echo "Monitoring dihentikan"
