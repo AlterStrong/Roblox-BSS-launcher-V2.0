f31d20098640f75fe6029e99b103d6312875f094
AlterStrong <kucing.lucuuuu12@gmail.com>
2025-08-07 20:57:34 +0700

    Update touch_ping.sh

--- touch_ping.sh
+++ touch_ping.sh
@@ -1,5 +1,3 @@
 #!/data/data/com.termux/files/usr/bin/bash
-# Sentuh file ping untuk hentikan spam ping sementara
-
-touch "$HOME/ping_active"
-echo "Sentuhan ping terkirim. Spam akan berhenti jika masih berlangsung."
+touch "$HOME/touch_ping.lock"
+echo "Touch ping lock diaktifkan"
