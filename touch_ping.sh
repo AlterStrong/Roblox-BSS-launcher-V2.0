a88dde349dfd093bc74c72824edb31009a3f107a
AlterStrong <kucing.lucuuuu12@gmail.com>
2025-08-07 20:38:49 +0700

    Create touch_ping.sh

---
+++ touch_ping.sh
@@ -0,0 +1,5 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# Membuat file indikator bahwa user sudah menyentuh/memerintah hentikan spam sementara
+
+touch "$HOME/touch_ping.txt"
+echo "Spam dihentikan sementara (touch_ping.txt dibuat)"
