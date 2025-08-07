67a6be739f9ba3c130b5af7443fa4c4051b83fcc
AlterStrong <kucing.lucuuuu12@gmail.com>
2025-08-07 20:50:52 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/data/data/com.termux/files/usr/bin/bash
+# Jalankan roblox_monitor.sh di background (jika belum jalan)
 
-chmod +x roblox_monitor.sh
-pkill -f roblox_monitor.sh > /dev/null 2>&1
-nohup ./roblox_monitor.sh > /dev/null 2>&1 &
-echo "[*] Monitoring Roblox dimulai..."
+if pgrep -f roblox_monitor.sh >/dev/null; then
+  echo "Script sudah berjalan."
+else
+  nohup bash "$HOME/roblox_monitor.sh" > /dev/null 2>&1 &
+  echo "Script roblox_monitor.sh dimulai."
+fi
